a8dedc7c0adaefff01981e6ce55cc4e755f45f5c
Rohi123569 <rt19592@gmail.com>
2024-12-28 12:01:18 +0530

    Utilities Folder
    Utilities

---
+++ DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SeatBookingProj.Utilities
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime StartOfWeek(this DateTime dt,DayOfWeek startDay = DayOfWeek.Monday)
+        {
+            int diff = (7 + (dt.DayOfWeek - startDay)) % 7;
+            return dt.AddDays(-1 * diff).Date;
+        }
+
+        public static DateTime EndOfWeek(this DateTime dt, DayOfWeek startDay = DayOfWeek.Monday)
+        {
+           
+            return dt.StartOfWeek(startDay).AddDays(6);
+        }
+    }
+}
